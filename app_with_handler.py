d30fc13ee3e18e775e86694e2f53efb9a0450999
kb0419tw <kb0419tw@gmail.com>
2024-09-26 11:31:14 +0800

    Update app_with_handler.py

--- app_with_handler.py
+++ app_with_handler.py
@@ -85,7 +85,14 @@ def handle_message(event):
             original_content_url='https://drive.google.com/file/d/1qsZmTBWYaDHN6whsICbqrJtFOWKkPg3h/view?usp=sharing',
             preview_image_url='https://drive.google.com/file/d/1qsZmTBWYaDHN6whsICbqrJtFOWKkPg3h/view?usp=sharing'
         )
-        line_bot_api.reply_message(event.reply_token, image_message)
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[image_message]
+                )
+            )
     else:
         # 创建一个 prompt，将 AI_GUIDELINES 和用户消息结合
         AI_GUIDELINES = '你現在是台灣新住民的專屬隨身問答助理，專注於幫助新來的居民解決生活中的各種問題。請根據用戶的語言選擇（繁體中文、簡體中文、英文、越南文等）回應他們的問題。你的回答應該簡單易懂，涵蓋以下幾個領域：1. 語言支持：回答用戶問題時，根據用戶的語言需求提供對應語言的回應。2. 生活資訊：提供有關台灣的日常生活、居住、交通、購物等資訊。例如你知道板橋的新住民家庭服務中心地點在新北市板橋區四川路2段16巷5號10樓，電話為0289668500;你也知道如果想要買電信儲值卡，可以到中華電信的下列INDEX門市，地址為臺北市大安區四維路198巷47號、臺北市萬華區青年路110號、臺北市萬華區國興路32號等。3. 文化習俗：介紹台灣的文化、節慶和習俗，幫助用戶適應本地文化。4. 就業機會：提供關於工作機會、求職技巧和職場文化的資訊。5. 醫療服務：解答關於醫療系統、就醫流程和健康保險的問題。6. 戶政手續：指導用戶如何辦理居留證、工作證等必要的官方文件。例如用戶想考汽機車駕照，你知道交通部公路局汽機車線上隨機模擬考系統的網址是https://www.mvdis.gov.tw/m3-simulator-drv/，能夠幫助用戶準備考試。7. 教育資源：提供有關學校、語言學習和其他教育資源的信息。每次回答時，請確保用語簡潔明瞭，並考慮到新住民可能對台灣的生活和制度不太熟悉。請根據用戶的具體需求，提供最合適的解決方案或建議。'
